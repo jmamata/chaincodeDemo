c914930d1607995e702ff9c6edd8f4404c107145
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2017-06-18 01:38:28 +0530

    Update chaincode.go

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -12,13 +12,19 @@ const   PREFIX_PATIENT =  "patient"
 type SimpleChaincode struct {
 }
 
-type Patient struct {
-	Name            	string 		`json:"name"`
-	Model           	string 		`json:",string"`
-	CurrentProblem      	string 		`json:"currentproblem"`
-	allergies      		string 		`json:"currentproblem"`
+type Prescription  struct {
+	Disease            	string 		`json:"disease"`
+	Medication          string 		`json:"medication"`
+	Duration      		string 		`json:"duration"`
 }
 
+type Patient struct {
+	Name            	string 			`json:"name"`
+	dob           		string 			`json:"dob"`
+	CurrentProblem      string 			`json:"currentproblem"`
+	allergies      		string 			`json:"currentproblem"`
+	Prescriptions 		[]Prescription 	`json:"prescriptions"`
+}
 
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
@@ -37,6 +43,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if function == "enter_patient_details" {
 		return t.enter_patient_details(stub, args)
+	}else if function == "enter_patient_prescription" {
+		return t.enter_patient_prescription(stub, args)
 	}
 
 	return nil, errors.New("Received unknown function invocation " + function)
@@ -62,8 +70,12 @@ func (t *SimpleChaincode) enter_patient_details(stub shim.ChaincodeStubInterface
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	var patient Patient
-	patient = Patient{args[0],args[1],args[2],args[3]}
+
+	patient := Patient{}
+	patient.Name = args[0]
+	patient.dob = args[1]
+	patient.CurrentProblem = args[2]
+	patient.allergies =  args[3]
 	bytes, err := json.Marshal(&patient)
 
 	if err != nil { 
@@ -92,4 +104,41 @@ func (t *SimpleChaincode) get_patient_details(stub shim.ChaincodeStubInterface, 
 	}
 
 	return patient, nil
+}
+
+func (t *SimpleChaincode) enter_patient_prescription(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	//		0		 1		   2		  3
+	//	   Name   Disease  Medication  Duration
+
+	if len(args) != 4 { 
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	}
+
+	bytes, err := stub.GetState(PREFIX_PATIENT + args[0])
+	if err != nil { 
+		return nil, errors.New("No Patient with name " + args[0])
+	}
+
+
+	prescription := Prescription{}
+	prescription.Disease = args[1]
+	prescription.Medication = args[2]
+	prescription.Duration = args[3]
+
+	var patient Patient
+ 	err = json.Unmarshal(bytes,&patient)
+ 	patient.Prescriptions = append(patient.Prescriptions, prescription)
+
+	bytes, err := json.Marshal(&patient)
+	if err != nil { 
+		return nil, errors.New("Error converting Patient record") 
+	}
+
+	err = stub.PutState(PREFIX_PATIENT + args[0], bytes)
+	if err != nil { 
+		return nil, errors.New("Error storing Patient record") 
+	}
+
+	return nil, nil
 }
