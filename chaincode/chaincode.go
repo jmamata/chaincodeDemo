2cfc6f76ab52c43f723d3a05a6b66761f2a9cb4e
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2017-06-17 10:13:49 +0530

    Update chaincode.go

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -16,17 +16,17 @@ type SimpleChaincode struct {
 type Patient struct {
 	Name            	string 		`json:"name"`
 	Model           	string 		`json:",string"`
-	CurrentProblem      string 		`json:"currentproblem"`
+	CurrentProblem      	string 		`json:"currentproblem"`
 	allergies      		string 		`json:"currentproblem"`
 }
 
 
 
-func (t *SampleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     return nil, nil
 }
  
-func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if function == "get_patient_details" {
 		return t.get_patient_details(stub, args)
 	}
@@ -35,7 +35,7 @@ func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 }
  
-func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if function == "enter_patient_details" {
 		return t.enter_patient_details(stub, args)
 	}
